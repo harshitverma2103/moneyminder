a3605733634d019a7182a9d7b1a03d5576027d06
harshitverma2103 <harshitverma2103@gmail.com>
2024-10-30 21:48:18 +0530

    created hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
           foreground: "hsl(var(--popover-foreground))",
         },
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#6666FF",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
