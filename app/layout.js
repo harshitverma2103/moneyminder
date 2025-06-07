e88f813a998a8c5241517d2a3a2987cbbc69129e
harshitverma2103 <harshitverma2103@gmail.com>
2025-06-07 15:40:23 +0530

    deployment issue fix

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider 
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      appearance={{
+        baseTheme: undefined
+      }}
+    >
       <html lang="en">
         <body className={`${outfit.variable} antialiased`}>
           <Toaster/>
